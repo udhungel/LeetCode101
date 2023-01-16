c8086e52a65aa5189d9569fbf64c024bc13f9fc1
udhungel <udeepdhungel@gmail.com>
2023-01-16 22:22:31 +0100

    LeetCode 290 - Word Pattern Full explanation how i solved it can be found on https://youtu.be/EhOwN82deCg

---
+++ Solution290.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode101
+{
+    public class Solution290
+    {
+
+        public bool WordPattern(string pattern, string s)
+        {
+            var strVal = getPattern(s.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)));
+
+            var patternVal = getPattern(pattern.Select(x => x.ToString()));
+
+
+            var isTrue = strVal.SequenceEqual(patternVal);
+
+            return isTrue;
+
+
+
+        }
+
+        private static string getPattern(IEnumerable<string> strVals)
+        {
+            var grpVals = strVals.Select((x, i) => new { val = x, idx = i })
+                                 .GroupBy(x => x.val).ToDictionary(x => x.Key, x => x.First().idx);
+
+            var retVal = string.Join(",", strVals.Select(x => grpVals[x]));
+
+
+            return retVal;
+        }
+
+    }
+}
