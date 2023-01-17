15d9d017c378e4503d50b4058664697351322d8c
udhungel <udeepdhungel@gmail.com>
2023-01-17 21:01:41 +0100

    LeetCode 14 -Longest Common Prefix Used  to find the intergar value maxlength max length intergar value to find the string using FirstOrDefault looped and found the prefix using substring (0,i) checked to see if it is there in all elements

---
+++ LongestCommonPrefix_14.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode101
+{
+    public static class LongestCommonPrefix_14
+    {
+        /*
+            * Example 1:
+
+            Input: strs = ["flower","flow","flight"]
+            Output: "fl"
+            Example 2:
+
+            Input: strs = ["dog","racecar","car"]
+            Output: ""
+            Explanation: There is no common prefix among the input strings.
+         */
+
+
+
+
+        public static string MaxLength1()
+        {
+            string[] strVals = { "flower", "flow", "flight" };
+            var maxlength = strVals.Max(x => x.Length);
+            string prefix; 
+            var maxlenthString = strVals.FirstOrDefault(x => x.Length == maxlength);  //find the max length elment 
+            for (int i = maxlength; i >= 0; i--)
+            {
+                prefix = maxlenthString.Substring(0,i);
+                if (strVals.All(x=>x.StartsWith(prefix)))
+                {
+                    return prefix;
+                }              
+
+            }
+            return ""; 
+        }
+    }
+}
