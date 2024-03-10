e51aa359964701fee0106e76915462ae4eee0b80
udhungel <udeepdhungel@gmail.com>
2024-03-10 22:03:18 +0100

    LeetCode 121: Best Time to Buy And Sell Stock

--- BestTimeToBuyAndSellStock_121.cs
+++ BestTimeToBuyAndSellStock_121.cs
@@ -12,33 +12,49 @@ namespace LeetCode101
             //Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
             //Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
-            int[] prices = { 7, 1, 5, 3, 6, 4 };
+            int[] prices = { 7,1,5,3,6,4 };
+            //       Days   1, 2,3, 4, 5, 6 
 
-            //What I want to do
-            // Note : Profit only happens when  : selling price is > buying Price             
-            // Iterate over the loop and see if the  p[i] > buyingPrice that mean there is no profit set the buyingPrice as p[i] 
-            // Else check for profit which is  prices[p] - buyingprice where (selling price at that time)
-
-            int buyingPrice = prices[0];
             int profit = 0;
+            int intialBuy = prices[0];
 
-            for (int p = 1; p < prices.Length; p++)
+            for (int i = 0; i < prices.Length; i++)
             {
-                if (prices[p] < buyingPrice)
+                if (prices[i] > intialBuy)
                 {
-                    buyingPrice = prices[p];
+                    intialBuy = prices[i];
                 }
                 else
                 {
-                    int currentProfit = prices[p] - buyingPrice;
+                    int currentProfit= prices[i] - intialBuy;
                     profit = Math.Max(profit, currentProfit);
                 }
 
             }
 
-            return profit;  
-            
+            return profit;
+            //What I want to do
+            // Part 1 
+            // In order to get the profit need to get the lowest price in a array
+            // Note : Profit only happens when  : selling price is > buying Price
+
+
+            // Part 2
+            // Iterate over the loop and see if the (//check lowest )  p[i] < buyingPrice that mean there is no profit set the buyingPrice as p[i] 
+            // Else check for profit which is  prices[p] - buyingprice where (selling price at that time)
+
+
+
+
+            //return 0;
 
         }
     }
 }
+
+
+
+
+
+
+
