6952e98c41d9e6d9b9d67e556220ff61ed9f5c48
udhungel <udeepdhungel@gmail.com>
2024-03-17 20:49:18 +0100

    Leetcode 122 : Best Time to Buy and Sell Stock II

---
+++ Best_Time_to_Buy_and_Sell_Stock_II.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode101
+{
+    public class Best_Time_to_Buy_and_Sell_Stock_II_122
+    {
+        public static int Best_Time_to_Buy_and_Sell_Stock()
+        {
+            int[] prices = { 7, 1, 5, 3, 6, 4 };
+            int profit = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                if (prices[i] < prices[i + 1])
+                {
+                    profit += prices[i + 1] - prices[i];
+                }
+            }
+            return profit;
+        }
+    }
+}
